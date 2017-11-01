568fa39f6f6faeb9c337173aa6a1b5556615b40e
Kevin <kcorso89@gmail.com>
2017-11-01 16:43:04 -0700

    pre-Lord Dave's help

---
+++ javascript/app.js
@@ -0,0 +1,120 @@
+setTimeout(rickRoll, 321000);
+
+function rickRoll(){
+console.log("help");
+window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+};
+
+var userGuess = .on(click)
+
+var myQuestions = [
+	{
+		question: "How many Pokemon are there?",
+
+		answers: {
+			a:'151',
+			b:'251',
+			c:'721',
+			d:'802'
+		},
+		correctAnswer: 'd'
+	},
+
+	{
+		question: "Who was Ash's first Pokemon?",
+	
+		answers: {
+			a:'Pikachu',
+			b:'Charmander',
+			c:'Squirtle',
+			d:'Bulbasaur',
+		},
+		correctAnswer: 'a'
+	},
+			question: "Which of these is not an official Smogon tier?",
+	
+		answers: {
+			a:'OU',
+			b:'BL',
+			c:'UU',
+			d:'Ubers',
+		},
+		correctAnswer: 'b'
+	}
+			question: "Who won the official 2016 Pokemon World Tournament in the Master's division?",
+	
+		answers: {
+			a:'Wolfe Glick',
+			b:'Sejun Park',
+			c:'Ryota Otsubo',
+			d:'Ray Rizzo',
+		},
+		correctAnswer: 'a'
+	}
+			question: "Which of these Pokemon is not available for use in Pokken Tournament?",
+	
+		answers: {
+			a:'Suicune',
+			b:'Medicham',
+			c:'Machamp',
+			d:'Chandelure',
+		},
+		correctAnswer: 'b'
+	}
+			question: "Who orginally came up with the idea for the GBA draft-style Pokemon league on YouTube?",
+	
+		answers: {
+			a:'aDrive',
+			b:'M4GNITUDE',
+			c:'ShadyPenguinn',
+			d:'PokeaimMD',
+		},
+		correctAnswer: 'b'
+	}
+			question: "As of today's date, October 31, 2017, which of these coaches has never been in the GBA?",
+	
+		answers: {
+			a:'Mega Mogwai',
+			b:'Kooper',
+			c:'CrimsonCBad',
+			d:'Thunderblunder777',
+		},
+		correctAnswer: 'd'
+	}
+			question: "In what city was the official 2017 Pokemon World Championships held?",
+	
+		answers: {
+			a:'Los Angeles',
+			b:'San Francisco',
+			c:'Anaheim',
+			d:'San Diego',
+		},
+		correctAnswer: 'c'
+	}
+			question: "Who was not an original host of the objectively greatest Pokemon podcast out there, KantoCast?",
+	
+		answers: {
+			a:'Red',
+			b:'Green',
+			c:'Blue',
+			d:'Yellow',
+		},
+		correctAnswer: 'b'
+	}
+			question: "As of today's date, October 31, 2017, only one Pokemon has been OU every single generation--what Pokemon is that?",
+	
+		answers: {
+			a:'Alakazam',
+			b:'Zapdos',
+			c:'Gengar',
+			d:'Starmie',
+		},
+		correctAnswer: 'c'
+	}
+]
+
+$('form').on('submit', function() {
+	 var data = $(this).serialize(); 
+	 console.log("data".data)
+});
